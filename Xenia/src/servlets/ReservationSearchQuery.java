94bf901365e345e156b406fe0b07a8b2f701e707
Patrick Luddy <pluddy@outlook.com>
2016-03-08 15:45:01 -0600

    added Gzip usage on search results? maybe?

--- Xenia/src/servlets/ReservationSearchQuery.java
+++ Xenia/src/servlets/ReservationSearchQuery.java
@@ -15,6 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import beans.ReservationSearchResultBean;
+import managers.GzipManager;
 import models.Hotel;
 import services.SearchService;
 
@@ -70,8 +71,15 @@ public class ReservationSearchQuery extends HttpServlet {
 		List<ReservationSearchResultBean> hotelBeans = SearchService.searchHotels(checkIn, checkOut, locationCity, Integer.parseInt(numberOfRooms), roomType, amenities);
 		
 		session.setAttribute("hotels", hotelBeans);
+		if (GzipManager.isGzipSupported(request)) {
+			response.setHeader("Content-Encoding", "gzip");
+			request.getRequestDispatcher("ReservationSearch.jsp").forward(request, response);
+		} else {
+			request.getRequestDispatcher("ReservationSearch.jsp").forward(request, response);
+		}
+
+			
 		
-		request.getRequestDispatcher("ReservationSearch.jsp").forward(request, response);
 		
 	}
 
